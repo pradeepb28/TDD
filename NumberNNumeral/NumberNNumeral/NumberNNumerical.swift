8fc597368b6eaa59702b1f4cef0c86cddca1c497
pradeepb28 <pradeepboorgu@gmail.com>
2016-02-23 14:57:45 -0600

    logic added - numberToRoman

--- NumberNNumeral/NumberNNumeral/NumberNNumerical.swift
+++ NumberNNumeral/NumberNNumeral/NumberNNumerical.swift
@@ -10,25 +10,51 @@ import Foundation
 public class NumberNNumerical {
     let magnitudeNumbers=[1000,900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
     let numericals=["M","CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
-        func convertRomanToArab(numerical:String)->Int {
-            if ((numerical as NSString).length > 0 || numerical != "") {
-                if numerical == "M" {
-                    return 1000
-                }else{
-                    return 0
-                }
-            }else{
-                return -1
+    let values:[(String,Int)] = [("M", 1000), ("CM", 900), ("D", 500), ("CD", 400), ("C",100), ("XC", 90), ("L",50), ("XL",40), ("X",10), ("IX", 9), ("V",5),("IV",4), ("I",1)]
+//        func convertRomanToArab(var numerical:String)->Int {
+//            if ((numerical as NSString).length > 0 || numerical != "") {
+//               numerical = numerical.uppercaseString
+//                for(var i=0;i<numericals.count;i++){
+//                    let pos = numerical.characters.indexOf(num[i])
+//                    print(pos)
+//                }
+//                
+//                    return 0
+//                
+//            }else{
+//                return -1
+//            }
+//        }
+    
+    func convertRomanToArabic(value:String) -> Int? {
+        var string = value
+        
+        
+        
+        var result = 0
+        
+        for (romanChars, arabicValue) in values {
+            let range = Range<String.Index>(start: romanChars.startIndex, end: romanChars.endIndex)
+            
+            while string.hasPrefix(romanChars) {
+                result = result + arabicValue
+                
+                string.removeRange(range)
             }
         }
+        
+        if (string.isEmpty) {
+            return result
+        }
+        
+        return nil
+    }
     func convertArabicToRoman(var number:Int) -> String {
         
         if number > 0 {
             var roman:String = ""
             var repeats:Int = 0
-  
-            //repeats = number/1
-            for (var x = 0; number > 0; x++){
+              for (var x = 0; number > 0; x++){
                 repeats = number/magnitudeNumbers[x]
                 for (var i=1;i<=repeats;i++){
                     roman = roman + numericals[x]
